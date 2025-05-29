70df277fcb5e0dc010f72fa969315a8844765e08
shishupalsahu <shishupalsahu13@gmail.com>
2025-05-30 00:51:55 +0530

    Create Schema for Backend

---
+++ app/provider.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+import React from 'react'
+
+function Provider({children}) {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+  return (
+    <ConvexProvider client={convex}>
+    <div>
+      {children}
+    </div>
+    </ConvexProvider>
+  )
+}
+
+export default Provider
---
+++ app/schema.js
@@ -0,0 +1,12 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        name: v.string(),
+        email: v.string(),
+        picture: v.string(),
+        credits: v.string(),
+        paymentId: v.optional(v.string())
+    })
+})
